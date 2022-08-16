8136c010c344d083d84d41a467c5bb0e544fde71
Евгения <Evgenia@MacBook-Pro-Evgenia.local>
2022-08-16 14:34:35 +0300

    Methods practice; Hoe to construct a method

---
+++ Choices/Program.cs
@@ -0,0 +1,21 @@
+﻿// Используем функции выбора
+
+class Program
+{
+    static void SelectRase() // Ничего не возвращает
+    {
+        string[] namesRase = {"Люди", "Эльфы", "Гномы", "Гоблины"};
+        for (int i = 0; i < namesRase.Length - 1; i++) // Выводим все, кроме последней (ради аккуратности оформления)
+        {
+            Console.Write(i + 1 + " - " + namesRase[i] + ", "); 
+        }
+        Console.Write(namesRase.Length + " - " + namesRase[namesRase.Length - 1] + ".");
+    }
+
+
+
+static void Main(string[] args)
+    {
+        SelectRase();
+    }
+}
